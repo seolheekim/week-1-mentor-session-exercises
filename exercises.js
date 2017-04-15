46ecc3a8e936a2883ac210800e0ba5255aba5291
Seolhee Kim <seolhee@upspringmedia.com>
2017-04-15 09:59:10 -1000

    done

--- exercises.js
+++ exercises.js
@@ -6,6 +6,24 @@
  * ie: "cat" => "tac"
  */
 
+function firstReverse(str){
+  var splitString = str.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+    return joinArray;
+}
+console.log(firstReverse("cat"));
+
+/*
+-------- another way ------------
+
+function firstReverse(str){
+    return str.split("").reverse().join("");
+}
+console.log(firstReverse("cat"));
+
+*/
+
  /** Function: alphaOrder
  * The function will take the str parameter being passed in and
  * return the string in alphabetical order
@@ -14,6 +32,24 @@
  * ie: "cake" => "acek"
  */
 
+function alphaOrder(str){
+  var splitString = str.split("");
+  var sortArray = splitString.sort();
+  var joinArray = sortArray.join("");
+  return joinArray;
+}
+console.log(alphaOrder("javascript"));
+
+/*
+-------- another way ------------
+
+function alphaOrder(str){
+    return str.split("").sort().join("");
+}
+console.log(alphaOrder("cats"));
+
+*/
+
  /** Function: vowelCount
  * The function will take the str parameter being passed in and
  * return the number of vowels in the string
@@ -21,6 +57,28 @@
  * @return {number} count of vowels
  * ie: "oreo" => 3
  */
+
+function vowelCount(str) 
+{
+  var vowels = "aeiou";
+  var count = 0;
+
+
+  for(var i = 0; i<str.length; i++)
+  {
+  
+    if (vowels.indexOf(str[i]) !== -1)
+    {
+      count += 1;
+    }
+    
+  }
+  return count;
+
+}
+console.log(vowelCount("i love ice cream"));
+
+
 
  /** Function: timeConvert
  * The function will take the str parameter representing the amount of minutes being passed in and
@@ -31,6 +89,17 @@
  * ie: 68 => 1:8
  */
 
+ function timeConvert(n) {
+var num = n;
+var hours = (num / 60);
+var rhours = Math.floor(hours);
+var minutes = (hours - rhours) * 60;
+var rminutes = Math.round(minutes);
+return rhours + ":" + rminutes;
+}
+
+console.log(timeConvert(200));
+
  /** Function: repeatString
  * The function will take in two parameters and repeat a given string (first argument)
  * num times (second argument). Return an empty string if num is a negative number
@@ -39,6 +108,11 @@
  * @return {string} repeated num times
  * i.e repeatString("money", 3) => "moneymoneymoney".
  */
+
+function repeatString(str, num){
+  return (new Array(num + 1)).join(str);
+
+}console.log(repeatString("work", 5));
 
 
 /**
